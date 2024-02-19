2192aa12fca6f0e13f76fb4cd740798c5dd19674
yghaemi <yashar.ghaemi@antecedent.us>
2024-02-19 15:32:14 -0500

    first commit

---
+++ src/container/FileUpload.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+
+const FileUpload = (props:any) => {
+    return (
+       <Grid {...props}>
+            Start
+       </Grid>
+    );
+};
+
+export default FileUpload;
